33fecf96139cc1d6aceecea873851a75bf3f7826
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2021-05-17 17:13:09 -0300

    Adicionando o método buscar no controller OrdemServico

--- src/main/java/com/maria/eduarda/os/osapi/controller/OrdemServicoController.java
+++ src/main/java/com/maria/eduarda/os/osapi/controller/OrdemServicoController.java
@@ -1,12 +1,16 @@
 package com.maria.eduarda.os.osapi.controller;
 
 import com.maria.eduarda.os.osapi.domain.model.OrdemServico;
+import com.maria.eduarda.os.osapi.domain.repository.OrdemServicoRepository;
 import com.maria.eduarda.os.osapi.domain.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -15,9 +19,27 @@ public class OrdemServicoController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServicoService;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico){
         return gestaoOrdemServicoService.criar(ordemServico);
+    }
+
+    @GetMapping
+    public List<OrdemServico> listar(){
+        return ordemServicoRepository.findAll();
+    }
+
+    @GetMapping("/{ordemServicoId}")
+    public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId){
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+
+        if(ordemServico.isPresent()){
+            return ResponseEntity.ok(ordemServico.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 }
